d9588543c1f8e02615384d9d538d488ee0341479
Lee Sue Hyun <hyunls33@dalsoft.co.kr>
2020-02-18 17:12:03 +0900

    header 토큰 추가
    header 토큰 추가

--- app/src/main/java/com/dalsoft/repeattest/RetrofitGeneratorTest.java
+++ app/src/main/java/com/dalsoft/repeattest/RetrofitGeneratorTest.java
@@ -1,14 +1,28 @@
 package com.dalsoft.repeattest;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitGeneratorTest {
     static OkHttpClient.Builder httpClient = new OkHttpClient.Builder()
+                .addInterceptor(new Interceptor() {
+                    @Override
+                    public Response intercept(Chain chain) throws IOException {
+                        Request.Builder requestBuilder = chain.request().newBuilder();
+
+                        requestBuilder.addHeader("Authorization", "사용할 토큰");
+
+                        return chain.proceed(requestBuilder.build());
+                    }
+                })
                 .connectTimeout(3, TimeUnit.SECONDS)
                 .readTimeout(3, TimeUnit.SECONDS)
                 .writeTimeout(3, TimeUnit.SECONDS);
--- app/src/main/java/com/dalsoft/repeattest/RetrofitServiceTest.java
+++ app/src/main/java/com/dalsoft/repeattest/RetrofitServiceTest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import io.reactivex.Observable;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.Query;
 
@@ -12,4 +13,8 @@ public interface RetrofitServiceTest {
     @Headers("Content-Type: application/json")
     @GET("/comments")
     Observable<ArrayList<Comments>> getTestData(@Query("postId") int postId);
+
+    @Headers("Content-Type: application/json")
+    @GET("/comments")
+    Observable<ArrayList<Comments>> getTestData(@Header("Authorization") String token, @Query("postId") int postId);
 }
